b22a0137c25673134d87f20a84aeced61a46df50
torrestor <torres.jovem@gmail.com>
2026-01-06 16:27:27 -0300

    RouterOutlet + acertos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,21 @@ import { Routes } from '@angular/router';
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { ListaPostagemComponent } from './paginas/lista-postagem/lista-postagem.component';
 import { AuthGuard } from './guards/auth.guard';
+import { LayoutComponent } from './componentes/layout/layout.component';
 export const routes: Routes = [
     {
         //=> url/registro
         path: "registro",component: RegistroComponent 
     },
     {
-        path:"posts", canActivate: [AuthGuard], component: ListaPostagemComponent
+        path: "",
+        component: LayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path:"posts", component: ListaPostagemComponent
+            }
+        ]
     }
     
 ];
